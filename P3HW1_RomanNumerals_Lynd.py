d42043033cbef8970a23e5f9491bec764780faa3
Lyndm4200 <43307813+Lyndm4200@users.noreply.github.com>
2018-09-26 16:47:12 -0400

    Add files via upload

---
+++ P3HW1_RomanNumerals_Lynd.py
@@ -0,0 +1,32 @@
+# CTI-110
+# P3HW1 - Roman Numerals
+# Marc Lynd 
+# September 26, 2018
+#
+# Ask user to input a number 1-10
+numEntered = int(input("Please enter a number 1-10: "))
+# Determine the number entered and display the corresponding roman numeral
+if numEntered == 1:
+    print("I")
+elif numEntered == 2:
+    print("II")
+elif numEntered == 3:
+    print("III")
+elif numEntered == 4:
+    print("IV")
+elif numEntered == 5:
+    print("V")
+elif numEntered == 6:
+    print("VI")
+elif numEntered == 7:
+    print("VII")
+elif numEntered == 8:
+    print("VIII")
+elif numEntered == 9:
+    print("IX")
+elif numEntered == 10:
+    print("X")
+else:
+    print("ERROR: NUMBER INPUT INVALID")
+#
+# End of program
